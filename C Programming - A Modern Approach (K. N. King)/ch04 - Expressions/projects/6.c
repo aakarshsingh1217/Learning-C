4b756f776364e929aac8202d6b0487437ced1572
Aakarsh <sngh15721@gmail.com>
2025-09-08 23:14:42 +0930

    Finished projects for chapter 4 - Expressions

--- C Programming - A Modern Approach (K. N. King)/ch04 - Expressions/projects/6.c
+++ C Programming - A Modern Approach (K. N. King)/ch04 - Expressions/projects/6.c
@@ -2,10 +2,21 @@
 
 int main(int argc, char *argv[])
 {
-    long long ean;
+    int num1, num2, num3, num4, num5, num6,
+        num7, num8, num9, num10, num11, num12;
 
     printf("Enter the first 12 digits of an EAN: ");
-    scanf("%d%d", &ean);
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", 
+        &num1, &num2, &num3, &num4, &num5, &num6,
+        &num7, &num8, &num9, &num10, &num11, &num12
+    );
+
+    int check_digit = 9 - (((
+        ((num2 + num4 + num6 + num8 + num10 + num12) * 3)
+        + (num1 + num3 + num5 + num7 + num9 + num11)
+        ) - 1) % 10);
+
+    printf("Check digit: %d\n", check_digit);
 
     return 0;
 }
